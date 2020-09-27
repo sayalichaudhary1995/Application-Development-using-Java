fe98bef4728b1c9256e96f181198d4da44fd85b6
sayalichaudhary1995 <chaudhary.sa@northeastern.edu>
2020-09-27 01:31:22 -0400

    Assignment 1

---
+++ Assignment1/src/Business/Person.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author sayu
+ */
+public class Person {
+    private String name;
+    private String geographicData;
+    private String dob;
+    private String telephoneNo;
+    private String faxNo;
+    private String email;
+    private String ssn;
+    private String medicalRecordNo;
+    private String healthPlanBeneficiaryNo;
+    private String bankaccountNo;
+    private String certificateNo;
+    private String vehicleIdentifier;
+    private String deviceIdentifier;
+    private String licensePlate;
+    private String linkedin;
+    private String ipAddress;
+    private String biometricIdentifier;
+    private String imageOfFace;
+    private String uniqueIdentifier;    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelephoneNo() {
+        return telephoneNo;
+    }
+
+    public void setTelephoneNo(String telephoneNo) {
+        this.telephoneNo = telephoneNo;
+    }
+
+    public String getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(String faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecordNo() {
+        return medicalRecordNo;
+    }
+
+    public void setMedicalRecordNo(String medicalRecordNo) {
+        this.medicalRecordNo = medicalRecordNo;
+    }
+
+    public String getHealthPlanBeneficiaryNo() {
+        return healthPlanBeneficiaryNo;
+    }
+
+    public void setHealthPlanBeneficiaryNo(String healthPlanBeneficiaryNo) {
+        this.healthPlanBeneficiaryNo = healthPlanBeneficiaryNo;
+    }
+
+    public String getBankaccountNo() {
+        return bankaccountNo;
+    }
+
+    public void setBankaccountNo(String bankaccountNo) {
+        this.bankaccountNo = bankaccountNo;
+    }
+
+    public String getCertificateNo() {
+        return certificateNo;
+    }
+
+    public void setCertificateNo(String certificateNo) {
+        this.certificateNo = certificateNo;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(String biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+
+    public String getImageOfFace() {
+        return imageOfFace;
+    }
+
+    public void setImageOfFace(String imageOfFace) {
+        this.imageOfFace = imageOfFace;
+    }
+
+    public String getUniqueIdentifier() {
+        return uniqueIdentifier;
+    }
+
+    public void setUniqueIdentifier(String uniqueIdentifier) {
+        this.uniqueIdentifier = uniqueIdentifier;
+    }
+    
+    
+}
